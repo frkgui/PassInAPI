c85a45981938202afe09cd99774b94b8c0b61dc8
Guilherme Frank <contatoguilhermefrank@gmail.com>
2024-04-02 10:45:56 -0300

    Add project files.

---
+++ PassIn.Api/Controllers/EventsController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using PassIn.Application.UseCases.Events.Register;
+using PassIn.Communication.Requests;
+
+namespace PassIn.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+
+public class EventsController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Register([FromBody] RequestEventJson request)
+    {
+        try
+        {
+            var useCase = new RegisterEventUseCase();
+
+            useCase.Execute(request);
+
+            return Created();
+        }catch (ArgumentException ex)
+        {
+
+        }
+    }
+}
---
+++ PassIn.Application/UseCases/Events/Register/RegisterEventUseCase.cs
@@ -0,0 +1,27 @@
+﻿using PassIn.Communication.Requests;
+
+namespace PassIn.Application.UseCases.Events.Register
+{
+    public class RegisterEventUseCase
+    {
+
+        public void Execute(RequestEventJson request)
+        {
+            Validate(request);
+        }
+
+        private void Validate(RequestEventJson request)
+        {
+            if (request.MaximumAttendees <= 0)
+            {
+                throw new ArgumentException("ERROR: The maximum attendees number is invalid.");
+            }
+
+            if (string.IsNullOrWhiteSpace(request.Title))
+            {
+                throw new ArgumentException("ERROR: Title is invalid or empty.");
+            }
+        }
+
+    }
+}
